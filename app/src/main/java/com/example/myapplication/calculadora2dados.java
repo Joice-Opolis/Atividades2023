56cf5162986ddcaab524e8c8de933f9142f17015
Joice Opolis <joice.opolis@gmail.com>
2023-03-21 21:34:48 -0300

    adicionado botão voltar para calculadora principal

--- app/src/main/java/com/example/myapplication/calculadora2dados.java
+++ app/src/main/java/com/example/myapplication/calculadora2dados.java
@@ -10,19 +10,31 @@ import android.widget.EditText;
 
 public class calculadora2dados extends AppCompatActivity {
     EditText resultado;
+    Button bvoltar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora2dados);
         resultado=findViewById(R.id.resultado);
+        bvoltar=findViewById(R.id.bvoltar);
 
         Intent it = getIntent();
         String resultado2 = it.getStringExtra("resultado");
         resultado.setText(resultado2);
 
+        bvoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(calculadora2dados.this,calculadora2.class);
+                startActivity(in);
+            }
+        });
+
 
     }
+
 }
 
 
